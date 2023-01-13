92349c1602c580de004bffa4ce4b2dff5f57c689
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-01-13 16:54:14 -0500

    carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,125 @@
             background-color: #B68400 !important;
             color: ;
         }
+
+        .carousel {
+            position: relative;
+            margin-bottom: 20px;
+            line-height: 1;
+        }
+
+        .carousel-indicators {
+            position: absolute;
+            top: 15px;
+            right: 15px;
+            z-index: 5;
+            list-style: none;
+        }
+
+        .carousel-indicators li {
+            display: block;
+            float: left;
+            width: 10px;
+            height: 10px;
+            margin-left: 5px;
+            text-indent: -999px;
+            background-color: #ab0a3d;
+            background-color: rgba(255, 255, 255, 0.25);
+            border-radius: 5px;
+        }
+
+        .carousel-indicators .active {
+            background-color: #ab0a3d;
+        }
+
+        .carousel-inner>.next,
+        .carousel-inner>.prev {
+            position: absolute;
+            top: 0;
+            width: 100%;
+        }
+
+        .carousel-inner>.next {
+            left: 100%;
+        }
+
+        .carousel-inner>.prev {
+            left: -100%;
+        }
+
+        .carousel-inner>.next.left,
+        .carousel-inner>.prev.right {
+            left: 0;
+        }
+
+        .carousel-inner>.active.left {
+            left: -100%;
+        }
+
+        .carousel-inner>.active.right {
+            left: 100%;
+        }
+
+
+        .carousel-control {
+            position: absolute;
+            top: 40%;
+            left: 15px;
+            width: 40px;
+            height: 40px;
+            margin-top: -20px;
+            font-size: 60px;
+            font-weight: 100;
+            line-height: 30px;
+            color: #ffffff;
+            text-align: center;
+            background: #222222;
+            border: 3px solid #ffffff;
+            -webkit-border-radius: 23px;
+            -moz-border-radius: 23px;
+            border-radius: 23px;
+            opacity: 0.5;
+            filter: alpha(opacity=50);
+        }
+
+        .carousel-control.right {
+            right: 15px;
+            left: auto;
+        }
+
+        .carousel-control:hover,
+        .carousel-control:focus {
+            color: #ffffff;
+            text-decoration: none;
+            opacity: 0.9;
+            filter: alpha(opacity=90);
+        }
+
+        .carousel-caption {
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            padding: 15px;
+            background: #333333;
+            background: rgba(68, 4, 18, 0.75);
+        }
+
+        .carousel-caption h4,
+        .carousel-caption p {
+            line-height: 20px;
+            color: #ffffff;
+        }
+
+        .carousel-caption h4 {
+            margin: 0 0 5px;
+        }
+
+        .carousel-caption p {
+            margin-bottom: 0;
+        }
+
+       
     </style>
 
 </head>
@@ -48,7 +167,7 @@
             <img src="img/logo_sedeso.png" alt="">
         </div>
 
-        <div class="row justify-content-end primario text-white my-1">
+        <div class="row justify-content-end primario text-white ">
             @if (Route::has('login'))
                 <div>
                     @auth
@@ -63,46 +182,46 @@
             @endif
         </div>
 
-        
-            <div id="carouselExampleCaptions" class="carousel slide p-0 " data-ride="carousel">
-              <ol class="carousel-indicators">
-                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-              </ol>
-              <div class="carousel-inner">
+        </div>
+
+        <div id="myCarousel" class="carousel slide " data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
                 <div class="carousel-item active w-100">
-                  <img class="d-block w-100" src="img/c22.jpg" alt="First slide" />
-                  <div class="carousel-caption d-none d-md-block secundario w-100">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </div>
+                    <img class="d-block w-100" src="{{ asset('img/c22.jpg') }}" alt="First slide" />
+                    <div class="carousel-caption ">
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="img/c33.jpg" alt="Second slide" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </div>
+
+                    <img class="d-block w-100" src="{{ asset('img/c33.jpg') }}" alt="Second slide" />
+                    <div class="carousel-caption">
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="img/c44.jpg" alt="Third slide" />
-                  <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                  </div>
+                    <img class="d-block w-100" src="{{ asset('img/c44.jpg') }}" alt="Third slide" />
+                    <div class="carousel-caption">
+                        <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </div>
                 </div>
-              </div>
-              <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            </div>
+            <a class="carousel-control left carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            </a>
+            <a class="carousel-control right carousel-control-next" href="#myCarousel" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
-              </a>
-            </div>
-
+            </a>
         </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
