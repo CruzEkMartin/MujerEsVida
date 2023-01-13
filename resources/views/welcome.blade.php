fd103ffcea066f4f432fc4c0031b2af0695caeae
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-01-13 15:39:08 -0500

    carrousel de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,56 +1,120 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <link rel="shortcut icon" href= "img/icono.ico"/>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Secretaría de Desarrollo Social - Portal HambreCero</title>
+<head>
+    <link rel="shortcut icon" href="img/icono.ico" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
-        <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <title>Secretaría de Desarrollo Social - Portal HambreCero</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-        <style>
-            .primario{
-              background-color: #ab0a3d !important;
-              color: #eaeaea !important;
-            }
-            .secundario{
-              background-color: #440412 !important;
-              color: ;
-            }
-          </style>
-      
-    </head>
-    <body>
-        <div class= "container-fluid">
-            <div class="row justify-content-between">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
-            </div>
-            
-            <div class="row justify-content-end primario text-white">
-                @if (Route::has('login'))
-                <div >
-                    @auth
-                        <a href="{{ url('/home') }}" >Inicio</a>
-                    @else
-                        <a href="{{ route('login') }}" >Administración</a>
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" >Register</a>
-                        @endif
-                    @endif
-                </div>
-            @endif
-            </div>
-            
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+
+    <style>
+        .primario {
+            background-color: #ab0a3d !important;
+            color: #eaeaea !important;
+        }
+
+        .secundario {
+            background-color: #440412 !important;
+            color: ;
+        }
+
+        .terciario {
+            background-color: #B68400 !important;
+            color: ;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-between">
+            <img src="img/logo_qroo.png" alt="">
+            <img src="img/logo_unidos.png" alt="">
+            <img src="img/logo_sedeso.png" alt="">
         </div>
 
+        <div class="row justify-content-end primario text-white my-1">
+            @if (Route::has('login'))
+                <div>
+                    @auth
+                        <a href="{{ url('/home') }}">Inicio</a>
+                    @else
+                        <a href="{{ route('login') }}">Administración</a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                @endif
+            </div>
+            @endif
+        </div>
+
+        
+            <div id="carouselExampleCaptions" class="carousel slide p-0 " data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active w-100">
+                  <img class="d-block w-100" src="img/c22.jpg" alt="First slide" />
+                  <div class="carousel-caption d-none d-md-block secundario w-100">
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="img/c33.jpg" alt="Second slide" />
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="img/c44.jpg" alt="Third slide" />
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+        </script>
+
     </body>
-</html>
+
+    </html>
